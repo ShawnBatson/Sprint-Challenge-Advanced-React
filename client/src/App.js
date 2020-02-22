d48251261118593c86554ee7c56344cb83d8ff75
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-02-21 18:50:52 -0600

    pushed version

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(players => this.setState({ players }))
+      .catch(err => console.log("No dogs :(", err));
+    console.log("componentDidMount running");
+    console.log(this.state);
+  }
+
+  render() {
+    console.log("rendering...");
+    return (
+      <div className="App">
+        <div className="players">
+          <div>
+            {this.state.players.map(player => (
+              <img width="200" src={player} key={player} alt={player} />
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
